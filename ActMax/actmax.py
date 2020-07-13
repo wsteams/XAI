18d4cd8391f232f8d28508563a49101875206a06
Sho Watari <53390550+sho-watari@users.noreply.github.com>
2020-07-13 21:10:01 +0900

    Update actmax.py

--- ActMax/actmax.py
+++ ActMax/actmax.py
@@ -1,6 +1,8 @@
 import cntk as C
 import numpy as np
 
+C.debugging.force_deterministic(0)
+
 img_channel = 3
 img_height = 224
 img_width = 224
@@ -11,31 +13,59 @@ beta = 1.0
 epsilon = 1e-5
 
 
+def convolution(weights, bias, pad=True, stride=1, name=''):
+    W = C.Constant(value=weights, name='W')
+    b = C.Constant(value=bias, name='b')
+
+    @C.BlockFunction('Convolution2D', name)
+    def conv2d(x):
+        return C.convolution(W, x, strides=[stride, stride], auto_padding=[False, pad, pad]) + b
+
+    return conv2d
+
+
+def dense(weights, bias, name=''):
+    W = C.Constant(value=weights, name='W')
+    b = C.Constant(value=bias, name='b')
+
+    @C.BlockFunction('Dense', name)
+    def fc(x):
+        return C.times(x, W) + b
+
+    return fc
+
+
 def vgg19(h):
     model = C.load_model("./VGG19_ImageNet_Caffe.model")
 
     params = model.parameters
     for i in range(16):
-        h = C.convolution(params[-(2 * i + 2)].value, h, strides=1, auto_padding=[False, True, True]) + params[-(2 * i + 1)].value
+        h = convolution(params[-(2 * i + 2)].value, params[-(2 * i + 1)].value)(h)
         h = C.relu(h)
         if i in [1, 3, 7, 11, 15]:
-            h = C.pooling(h, C.MAX_POOLING, pooling_window_shape=(2, 2), strides=(2, 2), auto_padding=[False, True, True])
+            h = C.layers.MaxPooling((2, 2), strides=2, pad=True)(h)
 
-    h = C.reshape(h, -1)
-    h = C.relu(C.times(h, params[4].value.reshape(-1, 4096)) + params[5].value)
-    h = C.relu(C.times(h, params[2].value) + params[3].value)
-    h = C.times(h, params[0].value) + params[1].value
+    h = C.relu(dense(params[4].value, params[5].value)(h))
+    h = C.relu(dense(params[2].value, params[3].value)(h))
+    h = dense(params[0].value, params[1].value)(h)
+
     return h
 
 
 if __name__ == "__main__":
+    #
+    # ImageNet categories
+    #
+    node = 65  # sea snake
+
+    #
+    # input and model
+    #
     input = C.input_variable(shape=(img_channel, img_height, img_width), dtype="float32", needs_gradient=True)
     
     model = vgg19(input - img_mean)
 
     img = np.ascontiguousarray((np.random.rand(img_channel, img_height, img_width) + img_mean, dtype="float32")
-
-    node = 65  # sea snake
 
     #
     # activation maximization
